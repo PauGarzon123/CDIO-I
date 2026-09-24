936ba1f54ca520b12cc2d099b06d847d63e591fa
PauGarzon123 <pau.garzon@estudiantat.upc.edu>
2026-09-24 13:49:07 +0200

    Error path Imatges

--- Tasca1/Matloplit.py
+++ Tasca1/Matloplit.py
@@ -1,9 +1,8 @@
 import matplotlib.pyplot as plt
 import rasterio
 
-imatge = '/home/archpc/CDIO-I/Tasca1/Imatges/S2C_31TDF_20250630_0_L2A_nir.tif'
-imatge_green = '/home/archpc/CDIO-I/Tasca1/Imatges/S2C_31TDF_20250630_0_L2A_green.tif'
-
+imatge = 'Tasca1/Imatges/S2C_31TDF_20250630_0_L2A_nir.tif'
+imatge_green = 'Tasca1/Imatges/S2C_31TDF_20250630_0_L2A_green.tif'
 #Imatge tiff comprimida,necessitem rasterio per descomprimir-ho a la mateixa resolucio per fer els calculs
 print(type(imatge))
 
